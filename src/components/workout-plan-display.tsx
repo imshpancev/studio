7a082bdc6f64e6757bb63a03b1cdcbe1b48789e3
Vyacheslav Shpantsev <Shpantsev.Vyacheslav@m.thelightech.com>
2025-07-28 09:42:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/workout-plan-display.tsx
+++ src/components/workout-plan-display.tsx
@@ -15,6 +15,7 @@ import { useRouter } from 'next/navigation';
 import { Sport } from '@/lib/workout-data';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from './ui/alert-dialog';
+import { Skeleton } from './ui/skeleton';
 
 
 type WorkoutPlanDisplayProps = {
@@ -127,88 +128,98 @@ export function WorkoutPlanDisplay({ data, onFinishPlan }: WorkoutPlanDisplayPro
             </TabsList>
              <Button onClick={() => setCurrentWeek(w => w + 1)} disabled={currentWeek === structuredPlan.length - 1} variant="outline" size="icon"><ChevronRight/></Button>
           </div>
+            
+            {selectedWeek ? (
+                <>
+                    <div className="my-4 p-4 border rounded-lg bg-muted/50">
+                        <h3 className="font-semibold flex items-center gap-2"><Target className="h-5 w-5 text-primary"/>Цель на неделю:</h3>
+                        <p className="text-muted-foreground">{selectedWeek.weekGoal}</p>
+                    </div>
 
-            <div className="my-4 p-4 border rounded-lg bg-muted/50">
-                <h3 className="font-semibold flex items-center gap-2"><Target className="h-5 w-5 text-primary"/>Цель на неделю:</h3>
-                <p className="text-muted-foreground">{selectedWeek.weekGoal}</p>
-            </div>
+                    <ScrollArea className="h-[60vh] pr-4">
+                      <Accordion type="single" collapsible defaultValue="item-0" className="w-full space-y-4">
+                        {selectedWeek.days.map((dayPlan, index) => (
+                          <AccordionItem value={`item-${index}`} key={index} className="border-b-0">
+                            <Card className="overflow-hidden rounded-lg">
+                               <AccordionTrigger className="p-4 hover:no-underline flex justify-between items-center w-full bg-muted/50 data-[state=open]:bg-muted">
+                                    <div className="text-left">
+                                        <p className="font-bold text-lg text-primary">{dayPlan.day}</p>
+                                        <p className="text-muted-foreground">{dayPlan.title}</p>
+                                    </div>
+                               </AccordionTrigger>
+                              <AccordionContent>
+                                <div className="p-4 border-t space-y-4">
+                                    {dayPlan.description && (
+                                         <Alert>
+                                            <AlertDescription>{dayPlan.description}</AlertDescription>
+                                        </Alert>
+                                    )}
 
-            <ScrollArea className="h-[60vh] pr-4">
-              <Accordion type="single" collapsible defaultValue="item-0" className="w-full space-y-4">
-                {selectedWeek.days.map((dayPlan, index) => (
-                  <AccordionItem value={`item-${index}`} key={index} className="border-b-0">
-                    <Card className="overflow-hidden rounded-lg">
-                       <AccordionTrigger className="p-4 hover:no-underline flex justify-between items-center w-full bg-muted/50 data-[state=open]:bg-muted">
-                            <div className="text-left">
-                                <p className="font-bold text-lg text-primary">{dayPlan.day}</p>
-                                <p className="text-muted-foreground">{dayPlan.title}</p>
-                            </div>
-                       </AccordionTrigger>
-                      <AccordionContent>
-                        <div className="p-4 border-t space-y-4">
-                            {dayPlan.description && (
-                                 <Alert>
-                                    <AlertDescription>{dayPlan.description}</AlertDescription>
-                                </Alert>
-                            )}
-
-                          {(dayPlan.exercises && dayPlan.exercises.length === 0) || dayPlan.title === "День отдыха" ? (
-                            <p className='text-muted-foreground'>Запланирован отдых. Восстановление — ключ к успеху!</p>
-                          ) : (
-                            <>
-                            <Button 
-                                variant="default" 
-                                size="sm" 
-                                onClick={() => handleStartWorkout(dayPlan)}
-                                className='w-full'
-                              >
-                               <Forward className="mr-2 h-4 w-4" /> Начать тренировку
-                             </Button>
-                            {dayPlan.exercises.map((exercise, exIndex) => (
-                            <div key={exIndex} className="flex gap-4 items-start pt-4 border-t">
-                              <div className="relative group w-[100px] h-[100px] flex-shrink-0">
-                                <Image
-                                  src={`https://placehold.co/100x100.png`}
-                                  data-ai-hint="exercise fitness"
-                                  alt={exercise.name}
-                                  width={100}
-                                  height={100}
-                                  className="rounded-lg object-cover w-full h-full"
-                                />
-                                <div className="absolute inset-0 flex items-center justify-center bg-black/40 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer">
-                                  <PlayCircle className="h-8 w-8 text-white" />
+                                  {(dayPlan.exercises && dayPlan.exercises.length === 0) || dayPlan.title === "День отдыха" ? (
+                                    <p className='text-muted-foreground'>Запланирован отдых. Восстановление — ключ к успеху!</p>
+                                  ) : (
+                                    <>
+                                    <Button 
+                                        variant="default" 
+                                        size="sm" 
+                                        onClick={() => handleStartWorkout(dayPlan)}
+                                        className='w-full'
+                                      >
+                                       <Forward className="mr-2 h-4 w-4" /> Начать тренировку
+                                     </Button>
+                                    {dayPlan.exercises.map((exercise, exIndex) => (
+                                    <div key={exIndex} className="flex gap-4 items-start pt-4 border-t">
+                                      <div className="relative group w-[100px] h-[100px] flex-shrink-0">
+                                        <Image
+                                          src={`https://placehold.co/100x100.png`}
+                                          data-ai-hint="exercise fitness"
+                                          alt={exercise.name}
+                                          width={100}
+                                          height={100}
+                                          className="rounded-lg object-cover w-full h-full"
+                                        />
+                                        <div className="absolute inset-0 flex items-center justify-center bg-black/40 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer">
+                                          <PlayCircle className="h-8 w-8 text-white" />
+                                        </div>
+                                      </div>
+                                      <div className="flex-1">
+                                         <div className="flex items-center gap-2">
+                                          <h4 className="font-semibold text-base">{exercise.name}</h4>
+                                          <TooltipProvider>
+                                            <Tooltip>
+                                              <TooltipTrigger asChild>
+                                                <Info className="h-4 w-4 text-muted-foreground cursor-pointer" />
+                                              </TooltipTrigger>
+                                              <TooltipContent className="max-w-xs">
+                                                <p className="font-bold">Описание</p>
+                                                <p>{exercise.description}</p>
+                                                <p className="font-bold mt-2">Техника выполнения</p>
+                                                <p>{exercise.technique}</p>
+                                              </TooltipContent>
+                                            </Tooltip>
+                                          </TooltipProvider>
+                                        </div>
+                                        <p className="text-muted-foreground text-sm">{exercise.details}</p>
+                                      </div>
+                                    </div>
+                                    ))}
+                                    </>
+                                  )}
                                 </div>
-                              </div>
-                              <div className="flex-1">
-                                 <div className="flex items-center gap-2">
-                                  <h4 className="font-semibold text-base">{exercise.name}</h4>
-                                  <TooltipProvider>
-                                    <Tooltip>
-                                      <TooltipTrigger asChild>
-                                        <Info className="h-4 w-4 text-muted-foreground cursor-pointer" />
-                                      </TooltipTrigger>
-                                      <TooltipContent className="max-w-xs">
-                                        <p className="font-bold">Описание</p>
-                                        <p>{exercise.description}</p>
-                                        <p className="font-bold mt-2">Техника выполнения</p>
-                                        <p>{exercise.technique}</p>
-                                      </TooltipContent>
-                                    </Tooltip>
-                                  </TooltipProvider>
-                                </div>
-                                <p className="text-muted-foreground text-sm">{exercise.details}</p>
-                              </div>
-                            </div>
-                            ))}
-                            </>
-                          )}
-                        </div>
-                      </AccordionContent>
-                    </Card>
-                  </AccordionItem>
-                ))}
-              </Accordion>
-            </ScrollArea>
+                              </AccordionContent>
+                            </Card>
+                          </AccordionItem>
+                        ))}
+                      </Accordion>
+                    </ScrollArea>
+                </>
+            ) : (
+                <div className="space-y-4">
+                    <Skeleton className="h-12 w-full" />
+                    <Skeleton className="h-24 w-full" />
+                    <Skeleton className="h-24 w-full" />
+                </div>
+            )}
         </Tabs>
       </CardContent>
     </Card>
