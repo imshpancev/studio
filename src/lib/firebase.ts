a084bc31b7d9c0cf9d32727fd9059f829c316c82
Vyacheslav Shpantsev <Shpantsev.Vyacheslav@m.thelightech.com>
2025-07-30 11:38:00 +0000

    Request URL https://6000-firebase-studio-1753678027760.cluster-3ch54x2ep

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  projectId: "optimumpulse",
+  projectId: "the-lighsport",
   appId: "1:257655082700:web:18ef8355c1a2f5e6f97c69",
-  storageBucket: "optimumpulse.appspot.com",
+  storageBucket: "the-lighsport.appspot.com",
   apiKey: "AIzaSyB1N-yohIDmxxFwqUJX4QT-F3qo40UAFB0",
-  authDomain: "optimumpulse.firebaseapp.com",
+  authDomain: "the-lighsport.firebaseapp.com",
   messagingSenderId: "257655082700",
 };
+
 
 // Initialize Firebase
 let app;
